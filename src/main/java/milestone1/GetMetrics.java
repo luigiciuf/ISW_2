6bdeae40c89a254ea7ac4d69e33561ec6b92a667
ciuffreda_luigi <92534853+luigiciuf@users.noreply.github.com>
2024-05-04 11:45:17 +0200

    getter and setter Instance added

--- src/main/java/milestone1/GetMetrics.java
+++ src/main/java/milestone1/GetMetrics.java
@@ -1,4 +1,73 @@
 package milestone1;
 
+import model.Commit;
+import model.Instance;
+import model.Version;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.Edit;
+import org.eclipse.jgit.revwalk.RevCommit;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static jdk.internal.org.jline.utils.DiffHelper.diff;
+
 public class GetMetrics {
+    private Git git;
+    private ArrayList<Instance> instances;
+
+    private GetMetrics(){
+        this.instances=new ArrayList<>();
+
+    }
+
+
+    public List<Instance> getInstances(Git git, List<Commit> commits, List<Version> versions, Map<String, List<Integer>> mapInst) throws IOException{
+        this.git=git;
+        ArrayList<Instance> temp= new ArrayList<>();
+        Map<String,Integer> mapTemp= new HashMap<>();
+        Instance instance= null;
+        RevCommit prevCommit= null;
+        Version version = versions.get(0);
+
+        for (Commit commit: commits){
+            //recuperiamo autore del commit
+            String author = commit.getAuthor();
+            //verifichiamo che il commit sia un commit di fix
+            boolean fixCommit= !commit.getBuggyTickets().isEmpty();
+            if(!version.getName().equals(commit.getVersion().getName())){
+                updateIstances(mapInst,temp,mapTemp);
+                version=commit.getVersion();
+                for(Instance t : temp){
+                    t.setVersion(version);
+                }
+            }
+            manageFiles(commit,prevCommit,temp,mapTemp,version,author,fixCommit);
+            int nCommTogheter = commit.getClassesTouched().size();
+            for (String file: commit.getClassesTouched()){
+                instance= temp.get(mapTemp.get(file));
+            }
+            prevCommit=commit.getRev();
+        }
+        updateInstances(mapInst,temp,mapTemp);
+        return instances;
+    }
+
+    private void manageFiles(Commit commit, RevCommit prevCommit, ArrayList<Instance> temp, Map<String, Integer> mapTemp, Version version, String author, boolean fixCommit) throws IOException{
+        Instance inst =null;
+        List<DiffEntry> listDe = diff(commit.getRev(), prevCommit);
+        for(String file : commit.getClasses()){
+            List<Edit> edits= getEdits
+        }
+
+    }
+
+    private   diff(RevCommit rev, RevCommit prevCommit) {
+    }
+
+
 }
--- src/main/java/model/Instance.java
+++ src/main/java/model/Instance.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 public class Instance {
 
-
     private String name;
     private Version version;
     private Date dateCreation;
@@ -53,20 +52,28 @@ public class Instance {
         this.bugginess=false;
     }
 
+    /**
+     * Aggiorna le linee di codice aggiunte e rimosse, il churn, e la dimensione.
+     * @param added Linee di codice aggiunte.
+     * @param deleted Linee di codice rimosse.
+     */
     public void updateInstanceLoc(int added, int deleted) {
         if (added > maxLocAdded) { // Se il numero di righe aggiunte è maggiore del massimo attuale
             maxLocAdded = added; // Aggiorna il massimo delle righe aggiunte
         }
 
-        locTouched += added + deleted; // Aggiungi le righe di codice toccate
+        locTouched += added + deleted; // Aggiorna le linee di codice toccate
 
-        int ch = added - deleted; // Calcola il churn
-        churn += ch; // Aggiorna il churn
-        if (ch > maxChurn) { // Se il churn attuale è maggiore del massimo attuale
-            maxChurn = ch; // Aggiorna il massimo churn
+        churn += added - deleted; // Calcola il churn e lo aggiorna
+        if ((added - deleted) > maxChurn) { // Se il churn attuale è maggiore del massimo attuale
+            maxChurn = added - deleted; // Aggiorna il massimo churn
         }
-        size += ch; // Aggiorna la dimensione della classe
+
+        size += added - deleted; // Aggiorna la dimensione della classe
+
+        LocAdded += added; // Aggiorna il totale delle linee di codice aggiunte
     }
+
 
     public boolean insideAV(Version iv, Version fv) {
         boolean flag = false; // Flag per indicare se l'istanza rientra nell'intervallo
@@ -76,76 +83,117 @@ public class Instance {
         return flag; // Restituisce se l'istanza è nell'intervallo
     }
 
-
-
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Version getVersion() {
         return version;
     }
 
+    public void setVersion(Version version) {
+        this.version = version;
+    }
 
     public Date getDateCreation() {
         return dateCreation;
     }
 
-
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
 
     public int getSize() {
         return size;
     }
 
-
+    public void setSize(int size) {
+        this.size = size;
+    }
 
     public int getLocTouched() {
         return locTouched;
     }
 
-
+    public void setLocTouched(int locTouched) {
+        this.locTouched = locTouched;
+    }
 
     public int getNR() {
         return NR;
     }
 
+    public void setNR(int NR) {
+        this.NR = NR;
+    }
 
     public int getNFix() {
         return NFix;
     }
 
+    public void setNFix(int NFix) {
+        this.NFix = NFix;
+    }
 
     public int getMaxLocTouched() {
         return maxLocTouched;
     }
 
-
+    public void setMaxLocTouched(int maxLocTouched) {
+        this.maxLocTouched = maxLocTouched;
+    }
 
     public int getChurn() {
         return churn;
+    }
+
+    public void setChurn(int churn) {
+        this.churn = churn;
     }
 
     public int getMaxChurn() {
         return maxChurn;
     }
 
+    public void setMaxChurn(int maxChurn) {
+        this.maxChurn = maxChurn;
+    }
+
     public int getAvgChurn() {
         return avgChurn;
     }
 
+    public void setAvgChurn(int avgChurn) {
+        this.avgChurn = avgChurn;
+    }
 
+    public int getLocAdded() {
+        return LocAdded;
+    }
+
+    public void setLocAdded(int locAdded) {
+        LocAdded = locAdded;
+    }
 
     public int getMaxLocAdded() {
         return maxLocAdded;
     }
 
+    public void setMaxLocAdded(int maxLocAdded) {
+        this.maxLocAdded = maxLocAdded;
+    }
 
     public boolean isBugginess() {
         return bugginess;
     }
+
     public void setBugginess(boolean bugginess) {
         this.bugginess = bugginess;
     }
+
 
 }
