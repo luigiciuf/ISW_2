0af19f54c9c77d803038a3193afc77c0431e0ec6
ciuffreda_luigi <92534853+luigiciuf@users.noreply.github.com>
2024-05-07 09:16:18 +0200

    remove code smell

--- src/main/java/Utils/JsonUtils.java
+++ src/main/java/Utils/JsonUtils.java
@@ -1,6 +1,5 @@
 package Utils;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -28,23 +27,7 @@ public class JsonUtils {
         }
         return sb.toString();
     }
-    /**
-     * Legge un JSONArray da un URL.
-     * @param url L'URL da cui leggere il JSON.
-     * @return Un JSONArray contenente i dati letti.
-     * @throws IOException Se si verifica un errore di I/O.
-     * @throws JSONException Se si verifica un errore durante il parsing del JSON.
-     */
-    public static JSONArray readJsonArrayFromUrl(String url) throws IOException, JSONException {
-        InputStream is = new URL(url).openStream();
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            return new JSONArray(jsonText);
-        } finally {
-            is.close();
-        }
-    }
+
     /**
      * Legge un JSONObject da un URL.
      * @param url L'URL da cui leggere il JSON.
