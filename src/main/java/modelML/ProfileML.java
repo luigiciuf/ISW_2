a9d4b76b7572a5485c951216dbb6dece26d96bc4
ciuffreda_luigi <92534853+luigiciuf@users.noreply.github.com>
2024-05-26 11:12:35 +0200

    remove code smell

--- src/main/java/modelML/ProfileML.java
+++ src/main/java/modelML/ProfileML.java
@@ -1,52 +1,48 @@
 package modelML;
 
-public class ProfileML {
-    // Costruttore privato per impedire l'istanza della classe
-    /**
-     * Costruttore privato che lancia un'eccezione per impedire l'istanza della classe.
-     * Questa classe è progettata per non essere istanziata.
-     */
+/**
+ * Questa classe contiene enumerazioni per classificatori, metodi di selezione delle caratteristiche,
+ * metodi di campionamento e sensibilità ai costi. Non deve essere istanziata.
+ */
+public abstract class ProfileML {
+
     private ProfileML() {
-        throw new IllegalStateException("ProfileML class must not be instantiated");
+        throw new UnsupportedOperationException("ProfileML class cannot be instantiated");
     }
 
-    // Enumerazione per i tipi di classificatori
     /**
      * Enumerazione per i diversi tipi di classificatori.
      */
-    public enum CLASSIF {
-        RANDOM_FOREST, // Classificatore Random Forest
-        NAIVE_BAYES,   // Classificatore Naive Bayes
-        IBK;           // Classificatore IBK (K-nearest neighbors)
+    public enum Classifier {
+        RANDOM_FOREST,
+        NAIVE_BAYES,
+        IBK
     }
 
-    // Enumerazione per i metodi di selezione delle caratteristiche
     /**
      * Enumerazione per i diversi metodi di selezione delle caratteristiche (Feature Selection).
      */
-    public enum FS {
-        NO_SELECTION, // Nessuna selezione delle caratteristiche
-        BEST_FIRST;   // Selezione delle migliori caratteristiche (Best First)
+    public enum FeatureSelection {
+        NO_SELECTION,
+        BEST_FIRST
     }
 
-    // Enumerazione per i metodi di campionamento
     /**
      * Enumerazione per i diversi metodi di campionamento.
      */
-    public enum SMP {
-        NO_SAMPLING,    // Nessun campionamento
-        OVERSAMPLING,   // Aumento del campione (Oversampling)
-        UNDERSAMPLING,  // Riduzione del campione (Undersampling)
-        SMOTE;          // Tecnica SMOTE (Synthetic Minority Over-sampling Technique)
+    public enum SamplingMethod {
+        NO_SAMPLING,
+        OVERSAMPLING,
+        UNDERSAMPLING,
+        SMOTE
     }
 
-    // Enumerazione per la sensibilità ai costi
     /**
      * Enumerazione per i diversi approcci alla sensibilità ai costi.
      */
-    public enum CS {
-        NO_COST_SENSITIVE,   // Nessuna sensibilità ai costi
-        SENSITIVE_THRESHOLD, // Soglia sensibile ai costi
-        SENSITIVE_LEARNING;  // Apprendimento sensibile ai costi
+    public enum CostSensitivity {
+        NO_COST_SENSITIVE,
+        SENSITIVE_THRESHOLD,
+        SENSITIVE_LEARNING
     }
 }
